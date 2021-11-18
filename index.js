69cdf6372dbe4a0de203bb5dedfaa31e70a26450
Matt <mattelliottus@gmail.com>
2021-11-18 08:05:29 -0800

    removed shadow root, added method addOption to add key value pair to super select object, changed styles a bit

--- index.js
+++ index.js
@@ -7,46 +7,52 @@
 class SuperSelect extends HTMLElement {
   constructor() {
     super();
-
-    // create shadow root (the hidden dom we attach all our functionality to)
-    var shadow = this.attachShadow({ mode: 'open' });
     this.wrapper = document.createElement('div');
     this.list = document.createElement('ul');
-    let defaultOption = document.createElement('li');
-    let style = document.createElement('style');
-    let arr = JSON.parse(this.getAttribute('text'));
-    let bgColor = this.getAttribute('background-color');
-    let hgColor = this.getAttribute('highlight-color');
-    let textColor = this.getAttribute('text-color');
-    let borderColor = this.getAttribute('border-color');
+    this.styleEl = document.createElement('style');
 
     this.list.classList = 'options';
+    let defaultOption = document.createElement('li');
     defaultOption.classList = 'active';
     defaultOption.setAttribute('disabled', true);
     defaultOption.innerText = '—— Select ——';
     this.list.prepend(defaultOption)
     this.wrapper.classList = 'super-select';
-    this.value = 'test';
+    this.value = '';
 
-    style.textContent = `
+    this.list.addEventListener('click', () => this.openSelect())
+    document.addEventListener('click', event => {
+      console.error(event.path.includes(this))
+      if (!event.path.includes(this)) {
+        this.closeSelect();
+      }
+    });
+    this.wrapper.append(this.list);
+  }
+
+  connectedCallback() {
+    console.error('erher')
+    this.styleEl.textContent = `
+      .super-select,
+      .super-select * {
+        box-sizing: border-box;
+      }
       .super-select {
-        width: auto;
-        margin: 0 auto;
-        background-color: ${bgColor};
-        color: ${textColor};
+        width: 100px;
+        margin: 0;
+        background-color: #f3f3f3;
+        color: #121212;
+        overflow: hidden;
       }
       .options {
         position: relative;
         padding: 8px;
-        border: 1px solid ${borderColor};
+        border: 1px solid #9f9f9f6b;
         border-radius: 5px;
         list-style: none;
         margin: 0;
         padding: 0;
         cursor: pointer;
-      }
-      .options li {
-        padding: 8px 0;
       }
       .options li[disabled=true]::after {
         content: 'V';
@@ -57,6 +63,9 @@ class SuperSelect extends HTMLElement {
         top: 0;
         left: 0;
         visibility: hidden;
+        max-width: 100%;
+        padding: 8px 4px;
+        text-overflow: ellipsis;
       }
       .options.open li,
       .options li.active {
@@ -65,19 +74,8 @@ class SuperSelect extends HTMLElement {
       }
     `;
 
-    arr.forEach(item => {
-      let li = document.createElement('li');
-      li.setAttribute('value', item.value)
-      li.innerText = item.key;
-      this.list.append(li);
-
-      li.addEventListener('click', event => this.changeValue(event))
-      this.list.addEventListener('click', () => this.openSelect())
-    });
-
-    this.wrapper.append(this.list);
-    shadow.appendChild(style);
-    shadow.appendChild(this.wrapper);
+    this.appendChild(this.styleEl);
+    this.appendChild(this.wrapper);
   }
 
   get value() {
@@ -85,6 +83,35 @@ class SuperSelect extends HTMLElement {
   }
   set value(data) {
     this.wrapper.setAttribute('value', `${data}`.trim());
+  }
+
+  addOption(data) {
+    let li = document.createElement('li');
+    let closeBtn = document.createElement('button');
+    li.setAttribute('value', data.value)
+    li.innerText = data.key;
+    closeBtn.classList = 'btn delete';
+    closeBtn.innerText = '(x)';
+    closeBtn.type = 'button';
+
+    li.append(closeBtn);
+    this.list.append(li);
+    let id = Array.from(this.list).length + 1;
+
+    closeBtn.addEventListener('click', event => {
+      event.preventDefault();
+      event.stopPropagation();
+      this.removeElement(id);
+    });
+    li.addEventListener('click', event => this.changeValue(event));
+  }
+
+  removeElement(id) {
+    console.log('remove', id)
+    this.querySelectorAll('li')[id].remove();
+    // this.querySelectorAll('li').forEach(el => {
+    //   if (el.key == key) { console.error(`kill ${el}`); el.remove(); }
+    // })
   }
 
   changeValue(event) {
@@ -98,8 +125,25 @@ class SuperSelect extends HTMLElement {
 
   openSelect() {
     this.list.classList.toggle('open');
+    this.opencb = this.closeSelect.bind(this);
+  }
+  closeSelect() {
+    console.error('close me!')
+    document.removeEventListener('click', this.opencb)
+    this.list.classList.remove('open');
   }
 }
 
 // define new element
 customElements.define('super-select', SuperSelect);
+
+// DEMO INIT
+window.onload = function () {
+  let select = document.createElement('super-select');
+  document.body.append(select);
+  select.addOption({ key: 'Option # 1', value: '1' });
+  select.addOption({ key: 'Option # 2', value: '2' });
+  select.addOption({ key: 'Option Number 3', value: '3' });
+  select.addOption({ key: 'Option # 4', value: '4' });
+
+}
